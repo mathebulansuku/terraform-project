6d3e1bd4211a139888321e369b755f020ae7682c
mathebulansuku <mathebulansuku@gmail.com>
2025-10-21 05:39:15 +0000

    added output block

--- EC2/main.tf
+++ EC2/main.tf
@@ -2,22 +2,32 @@ provider "aws" {
   region = "af-south-1"
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "amiID" {
   most_recent = true
 
   filter {
     name = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["099720109477"] 
 }
 
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+# resource "aws_instance" "app_server" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "learn-terraform"
-  }
+#   tags = {
+#     Name = "learn-terraform"
+#   }
+# }
+
+output "instance_id" {
+  description = "AMI ID of ubuntu instance"
+  value = data.aws_ami.amiID.id
+  
 }
